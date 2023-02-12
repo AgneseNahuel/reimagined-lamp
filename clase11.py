48ecce96a8afe31a88a968f5a013e1c291edff35
Nahuel Nicolas Agnese <117863450+AgneseNahuel@users.noreply.github.com>
2023-02-12 18:09:50 -0300

    12/2/23

--- clase11.py
+++ clase11.py
@@ -1,7 +1,23 @@
 #Operaciones con condición en columnas | Pandas | Python |
 
+import pandas as pd
+datos=pd.read_csv('minutos2.csv')
+df=pd.DataFrame(datos)
 
+alto=df.loc[0, 'B']
 
+for i in range(1, len(df)):
+    #loc va a permitirnos usar matematicas, si seria iloc seria imposible solo manejar los titulos
+    if df.loc[i, 'B']>df.loc[i-1, 'B'] and df.loc[i, 'B']>alto:
+        df.loc[i, 'dB']=df.loc[i, 'B']-alto
+        alto=df.loc[i, 'B']
+    else:
+        df.loc[i, 'dB']=0
+
+print(df)
+
+total=df['dB'].sum()
+print("El total es: ", total)
 
 
 
---
+++ clase12.py
@@ -0,0 +1,51 @@
+#Agrupar datos |Groupby | Pandas | Python |
+
+import pandas as pd
+
+datos=pd.read_csv('movies2.csv')
+df=pd.DataFrame(datos)
+print("Maxima duracion********")
+
+#Devolucion de columna con la funcion max
+print(df['duration'].max())
+
+#Pasando minutos a horas
+minutos=df['duration'].max()
+print('Horas: ', minutos/60)
+
+#Devuelve el indice maximo
+print(df['duration'].idxmax())
+
+#Para el nombre
+#print(df.get_values(df['duration'].idxmax(), 'movie_title'))
+
+print("Comparacion de likes entre generos*****")
+print("\n"*2)
+#groupby compara (estudiarlo)
+df1=df.groupby(['genres'])[['movie_facebook_likes']].sum()
+print(df1)
+print("\n"*2)
+
+print("genero mas buscado: ")
+likes=df.groupby(['genres'])[['movie_facebook_likes']].sum()
+print(likes['movie_facebook_likes'].idxmax(), end='')
+print(' con: ', likes['movie_facebook_likes'].max(), ' likes')
+
+print("Promedio gastado en las peliculas*******")
+df2=df.groupby(['director_name'])[['budget']].sum()
+df2=df.groupby(['director_name'])[['budget']].mean()
+print(df2)
+print("\n"*2)
+
+
+print("¿Cual genero gasta mas dinero?")
+print("\n"*2)
+dinero=df.groupby(['genres'])[['budget']].sum()
+print(dinero)
+print("el genero: ", dinero['budget'].idxmax(), " con ", dinero['budget'].max())
+
+
+
+
+
+
